7a2462c87c60fbc8d3ff644da053a860308c58a7
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-12-28 19:25:32 +0100

    Update minefield_spec.rb

--- spec/minefield_spec.rb
+++ spec/minefield_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Field do
   describe '.initialize' do
     it('initializes with right mine count') do
       field = Minefield.new(10, 10, 15)
-      expect field.mine_count to eq 15
+      expect(field.mine_count).to eq 15
     end
   end
 end

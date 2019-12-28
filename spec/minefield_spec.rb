2bff87a2a003cb2a4bccb41cace3f9399eef044f
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-12-28 19:46:26 +0100

    Update minefield_spec.rb

--- spec/minefield_spec.rb
+++ spec/minefield_spec.rb
@@ -2,11 +2,19 @@
 
 require 'minefield'
 
-RSpec.describe Field do
+RSpec.describe Minefield do
   describe '.initialize' do
     it('initializes with right mine count') do
-      field = Minefield.new(10, 10, 15)
-      expect(field.mine_count).to eq 15
+      minefield = Minefield.new(10, 10, 15)
+      expect(minefield.mine_count).to eq 15
+    end
+  end
+  describe '.fill_map_with_fields' do
+    it('creates hash with fields') do
+      minefield = Minefield.new(10, 10, 15)
+      minefield.fill_map_with_fields
+      expect(minefield.board.class).to be Hash
+      expect(minefield.board.size).to eq 100
     end
   end
 end

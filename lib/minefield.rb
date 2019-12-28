a58f96fc91f33ca94574baf996faa5666fbd5b74
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-12-28 19:15:49 +0100

    Update minefield.rb

--- lib/minefield.rb
+++ lib/minefield.rb
@@ -5,6 +5,8 @@ class Minefield
   attr_accessor :width, :height, :mine_count
 
   def initialize(width, height, mine_percent)
-
+    @width = width
+    @height = height
+    @mine_count = width * height * mine_percent * 0.01
   end
 end

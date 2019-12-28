af971d58a508eac62338a0ca9adb68c8769a59e6
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-12-28 19:56:11 +0100

    Update minefield.rb

--- lib/minefield.rb
+++ lib/minefield.rb
@@ -2,11 +2,20 @@
 
 # Class for keeping game board
 class Minefield
-  attr_accessor :width, :height, :mine_count
+  attr_accessor :width, :height, :mine_count, :board
 
   def initialize(width, height, mine_percent)
     @width = width
     @height = height
     @mine_count = width * height * mine_percent * 0.01
+  end
+
+  def fill_map_with_fields()
+    @board = Hash.new
+    for i in 1..@width do
+      for j in 1..@height do
+        board[[i,j]] = Field.new
+      end
+    end
   end
 end

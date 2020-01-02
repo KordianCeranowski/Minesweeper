640e70d5e6d9cd7c735669f2dc483d0710c68aa2
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-01-02 12:01:38 +0100

    update cell_spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Cell do
       expect(cell.has_mine).to be false
     end
   end
+  describe '.plant_bomb' do
+    it('sets .has_mine to true') do
+      cell = Cell.new
+      expect(cell.has_mine).to be false
+      cell.plant_bomb
+      expect(cell.has_mine).to be true
+    end
+  end
+  describe '.show' do
+    it('shows the state of @hidden') do
+      cell = Cell.new
+      expect(cell.has_mine).to be false
+      cell.plant_bomb
+      expect(cell.has_mine).to be true
+    end
+  end
 end

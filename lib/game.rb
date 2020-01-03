9b866da3e52e19475fc8006330b5d92512fa7d27
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2020-01-03 02:41:22 +0100

    refactor game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -4,59 +4,77 @@ require_relative 'minefield'
 
 # Class for running a game
 class Game
-  def self.start
-    # puts('Jak duża mapa?')
-    # puts('- ile rzędów?: ')
-    # rows = gets.chomp
-    # puts('- ile kolumn?: ')
-    # columns = gets.chomp
-    # print('Ile procent min?: ')
-    # mines = gets.chomp
+  attr_accessor :board, :round_counter
 
-    # board = Minefield.new(rows, columns, mines)
-    board = Minefield.new(10, 10, 15)
-# ilość pól od 3 do 26
-    round_counter = 1
+  def self.start
+    @board = Minefield.new(13, 13, 15)
+
     print "\n----------------------\n"
     print "--- FIRST ROUND ---"
     print "\n----------------------\n"
-    round_counter += 1
-    board.print_empty_board
+    @board.print_empty_board
 
     print('Choose field to uncover: ')
     first_field = Game.letters_to_coordinates(gets.chomp)
     print("\n")
 
-    board.prepare_board(first_field)
-    board.refresh_visibility
-    board.print_board
+    @board.prepare_board(first_field)
+    @board.refresh_visibility
+    @board.print_board
 
-    # game loop
-    until board.game_lost
+    Game.loop_game
+  end
 
-      print('Choose field to uncover: ')
-      field = Game.letters_to_coordinates(gets.chomp)
-      print("\n")
-
-      board.uncover(field)
-      board.refresh_visibility
-      if board.game_lost
-        print "\n----------------------\n"
-        print "--- ): YOU LOST :( ---"
-        print "\n----------------------\n"
-        board.print_lost_board(field)
+  def self.loop_game
+    round_counter = 2
+    until @board.game_lost
+      field = Game.read_coordinates
+      @board.uncover(field)
+      @board.refresh_visibility
+      if @board.game_lost
+        Game.lose(field)
       else
-        print "\n----------------\n"
-        print "--- ROUND " + round_counter.to_s + " ---"
-        print "\n----------------\n"
-        round_counter += 1
-        board.print_board
+        Game.continue_playing(round_counter)
       end
     end
   end
 
+  def self.continue_playing(round_counter)
+    print "\n----------------\n"
+    print '--- ROUND ' + round_counter.to_s + ' ---'
+    print "\n----------------\n"
+    round_counter += 1
+    @board.print_board
+  end
+
+  def self.lose(field)
+    print "\n----------------------\n"
+    print '--- ): YOU LOST :( ---'
+    print "\n----------------------\n"
+    @board.print_lost_board(field)
+  end
+
+  def self.read_coordinates
+    print('Choose field to uncover: ')
+    field = Game.letters_to_coordinates(gets.chomp)
+    print("\n")
+    field
+  end
+
   def self.letters_to_coordinates(text)
     text.upcase.split('').map { |letter| letter.ord - 65 }
+  end
+
+  def self.minefield_from_input
+    # ilość pól od 3 do 26
+    puts('Podaj wymiary mapy')
+    puts('- ile rzędów?: ')
+    rows = gets.chomp
+    puts('- ile kolumn?: ')
+    columns = gets.chomp
+    print('Ile procent min?: ')
+    mines = gets.chomp
+    Minefield.new(rows, columns, mines)
   end
 end
 
